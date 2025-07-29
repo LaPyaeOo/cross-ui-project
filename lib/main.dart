4815a1f90337934135284f33481b55b95a6facaf
LaPyae <lapyaeoo@ayainnovation.com>
2025-07-29 15:25:03 +0630

    feat: layout for wide screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'destination.dart';
+// Remove the destination.dart import, it's not required
 import 'models/data.dart' as data;
 import 'models/models.dart';
+import 'widgets/disappearing_bottom_navigation_bar.dart';  // Add import
+import 'widgets/disappearing_navigation_rail.dart';        // Add import
 import 'widgets/email_list_view.dart';
 
 void main() {
@@ -37,38 +39,62 @@ class _FeedState extends State<Feed> {
     _colorScheme.surface,
   );
 
-  int selectedIndex = 0;                       // Add this variable
+  int selectedIndex = 0;
+  // Add from here...
+  bool wideScreen = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final double width = MediaQuery.of(context).size.width;
+    wideScreen = width > 600;
+  }
+  // ... to here.
 
   @override
   Widget build(BuildContext context) {
+    // Modify from here...
     return Scaffold(
-      body: Container(
-        color: _backgroundColor,
-        child: EmailListView(
-          // Add from here...
-          selectedIndex: selectedIndex,
-          onSelected: (index) {
-            setState(() {
-              selectedIndex = index;
-            });
-          },
-          // ... to here.
-          currentUser: widget.currentUser,
-        ),
+      body: Row(
+        children: [
+          if (wideScreen)
+            DisappearingNavigationRail(
+              selectedIndex: selectedIndex,
+              backgroundColor: _backgroundColor,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
+          Expanded(
+            child: Container(
+              color: _backgroundColor,
+              child: EmailListView(
+                selectedIndex: selectedIndex,
+                onSelected: (index) {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
+                currentUser: widget.currentUser,
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: wideScreen
+          ? null
+          : FloatingActionButton(
         backgroundColor: _colorScheme.tertiaryContainer,
         foregroundColor: _colorScheme.onTertiaryContainer,
         onPressed: () {},
         child: const Icon(Icons.add),
       ),
-      // Add from here...
-      bottomNavigationBar: NavigationBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        destinations: destinations.map<NavigationDestination>((d) {
-          return NavigationDestination(icon: Icon(d.icon), label: d.label);
-        }).toList(),
+      bottomNavigationBar: wideScreen
+          ? null
+          : DisappearingBottomNavigationBar(
         selectedIndex: selectedIndex,
         onDestinationSelected: (index) {
           setState(() {
@@ -76,7 +102,7 @@ class _FeedState extends State<Feed> {
           });
         },
       ),
-      // ...to here.
     );
+    // ... to here.
   }
 }
